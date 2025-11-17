21a4276695bf811f74eb4345521b96b446f17d3b
xefDavid <chefdavid@hotmail.com>
2025-11-17 13:44:57 +0100

    speed and separation monitoring

--- src/bumperbot_utils/bumperbot_utils/safety_stop.py
+++ src/bumperbot_utils/bumperbot_utils/safety_stop.py
@@ -3,8 +3,10 @@ import math
 from enum import Enum
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionClient
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
+from twist_mux_msgs.action import JoyTurbo
 
 #Creamos una variable enum para manejar el estado del robot
 class State(Enum):
@@ -20,11 +22,13 @@ class SafetyStop(Node):
 
     #vamos a crear los parametros del nodo...
         self.declare_parameter("danger_distance", 0.2) #marca la distancia minima antes de parar..20cm.
+        self.declare_parameter("warning_distance", 0.6)
         self.declare_parameter("scan_topic", "scan")        
         self.declare_parameter("safety_stop_topic","safety_stop")
     #Guardamos en las varibles los valores que recibimos de los paramentros y estandarizamos el tipo de valor
     #Para que los valores solo tengan dos decimales
         self.danger_distance = self.get_parameter("danger_distance").get_parameter_value().double_value 
+        self.warning_distance = self.get_parameter("warning_distance").get_parameter_value().double_value 
     #Para estandarizar el valor de scan topic a string
         self.scan_topic = self.get_parameter("scan_topic").get_parameter_value().string_value
     #Para  estandarizar el valor de safety_stop_topic a string
@@ -35,8 +39,19 @@ class SafetyStop(Node):
     #Creamos un publisher para decir si es true o false y si tiene que pararse o no el robot
         self.safety_stop_pub = self.create_publisher(Bool, self.safety_stop_topic, 10)
 
+    # Creamos dod  objetos para manejar el warning y reducir o aumentar la speed
+        self.decrease_speed_client = ActionClient(self,JoyTurbo, "joy_turbo_decrease")
+        self.increase_speed_client = ActionClient(self, JoyTurbo, "joy_turbo_increase")
+
+    # Comprobaciones para ver si el servido está activo , si no continua el node..
+        while not self.decrease_speed_client.wait_for_server(timeout_sec = 1.0) and rclpy.ok():
+            self.get_logger().warm("Action /joy_turbo_decrease not avalaible!! Waiting!!..")
+        while not self.increase_speed_client.wait_for_server(timeout_sec = 1.0) and rclpy.ok():
+                    self.get_logger().warm("Action /joy_turbo_increase not avalaible!! Waiting!!..")
+
     #Inicializamos el estadod del robot em free
         self.state = State.FREE
+        self.prev_state = State.FREE
 
 
     def laser_callback(self, msg: LaserScan):
@@ -45,18 +60,27 @@ class SafetyStop(Node):
         #Este loop revisa los valores para decidir si está dentro del rango Danger y parar el robot
         for range_value in msg.ranges:
             #si el valor es infinito o es menor a el valor marcado de danger_distance
-            if not math.isinf(range_value) and range_value <= self.danger_distance:
-                self.state = State.DANGER
-                break
+            if not math.isinf(range_value) and range_value <= self.warning_distance:
+                self.state = State.WARNING
+                if range.value <= self.danger_distance:
+                    self.state = State.DANGER
+                    break
 
-        is_safety_stop = Bool()
-        #Si el valor es exactamente igual a DANGER => is_safety_stop.data = True...
-        if self.state == State.DANGER:
-            is_safety_stop.data = True
-        elif self.state == State.FREE:
-             is_safety_stop.data = False
+        if self.state != self.prev_state:
+            is_safety_stop = Bool()
+            #Si el valor es exactamente igual a DANGER => is_safety_stop.data = True...
+            if self.state == State.WARNING:
+                 is_safety_stop.data = False
+                 self.decrease_speed_client.send_goal_async(JoyTurbo.Goal())
+            elif self.state == State.DANGER:
+                is_safety_stop.data = True
+            elif self.state == State.FREE:
+                is_safety_stop.data = False
+                self.increase_speed_client.send_goal_async(JoyTurbo.Goal())
 
-        self.safety_stop_pub.publish(is_safety_stop)
+            self.prev_state = self.state
+            self.safety_stop_pub.publish(is_safety_stop)
+
 def main():
     rclpy.init()
     node = SafetyStop()
